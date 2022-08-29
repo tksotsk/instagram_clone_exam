7ae44d372f0a84c2540a0db173ea0ac836c33093
takashi otsuka <shoshi100010000@gmail.com>
2022-08-29 11:23:21 +0900

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.perform_deliveries = true
+  
 end
